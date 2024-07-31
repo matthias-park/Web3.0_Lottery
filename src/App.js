2fde7e39823c648d8e465e7a0aaf9baebb1b105e
Junsoo Park <matthiaspark0514@gmail.com>
2024-07-31 10:43:38 -0700

    Feat: The Enter form

--- src/App.js
+++ src/App.js
@@ -5,21 +5,36 @@ import web3 from "./web3";
 import lottery from "./lottery";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { manager: "", players: [], balance: "", value: '' };
 
-    this.state = {manager: ''};
-  }
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
-    
-    this.setState({manager})
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({ manager, players, balance });
   }
   render() {
     return (
       <div>
         <h2>Lottery</h2>
-        <p>This contract is managed by {this.state.manager}</p>
+        <p>
+          This contract is managed by {this.state.manager}
+          There are currently {this.state.players.length} people enterd,
+          competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
+        </p>
+        <hr />
+        <form>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label> Amount of ether to enter</label>
+            <input 
+              value={this.state.value}
+              onChange={event => this.setState({value: event.target.value})}
+            />
+          </div>
+          <button>Enter</button>
+        </form>
       </div>
     );
   }
