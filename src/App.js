8efb2e85d380e5ceb818fe7dc33a8c44a385b819
Junsoo Park <matthiaspark0514@gmail.com>
2024-07-31 11:23:42 -0700

    Feat; Refactoring from class to functional component

--- src/App.js
+++ src/App.js
@@ -1,71 +1,80 @@
-import logo from "./logo.svg";
-import "./App.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import web3 from "./web3";
 import lottery from "./lottery";
 
-class App extends React.Component {
-  state = { manager: "", players: [], balance: "", value: '', message: '' };
+function App() {
+  const [manager, setManager] = useState("");
+  const [players, setplayers] = useState([]);
+  const [balance, setBalance] = useState("");
+  const [value, setValue] = useState("");
+  const [message, setMessage] = useState("");
 
-  async componentDidMount() {
-    const manager = await lottery.methods.manager().call();
-    const players = await lottery.methods.getPlayers().call();
-    const balance = await web3.eth.getBalance(lottery.options.address);
+  useEffect(() => {
+    async function fetchData() {
+      const manager = await lottery.methods.manager().call();
+      const players = await lottery.methods.getPlayers().call();
+      const balance = await web3.eth.getBalance(lottery.options.address);
 
-    this.setState({ manager, players, balance });
-  }
+      setManager(manager);
+      setplayers(players);
+      setBalance(balance);
+    }
 
-  onSubmit = async (event) => {
+    fetchData();
+  }, []);
+
+  async function handleSubmit(event) {
     event.preventDefault();
     const accounts = await web3.eth.getAccounts();
 
-    this.setState({ message: 'Waiting on transaction success...'});
+    setMessage("Waiting on transaction success...");
 
     await lottery.methods.enter().send({
       from: accounts[0],
-      value: web3.utils.toWei(this.state.value, 'ether'),
-    })
+      value: web3.utils.toWei(value, "ether"),
+    });
 
-    this.setState({message: 'You have been entered!'});
-  };
+    setMessage("You have been entered!");
+  }
 
-  onClick = async () => {
+  async function handleClick() {
     const accounts = await web3.eth.getAccounts();
-    this.setState({message: 'Waiting on transaction success...'})
+
+    setMessage("Waiting on transaction success...");
+
     await lottery.methods.pickWinner().send({
       from: accounts[0],
-    })
-    this.setState({message: 'A winnter has been picked!'});
+    });
+
+    setMessage("A winnter has been picked!");
   }
 
-  render() {
-    return (
-      <div>
-        <h2>Lottery</h2>
-        <p>
-          This contract is managed by {this.state.manager}
-          There are currently {this.state.players.length} people enterd,
-          competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
-        </p>
-        <hr />
-        <form onSubmit={this.onSubmit}>
-          <h4>Want to try your luck?</h4>
-          <div>
-            <label> Amount of ether to enter</label>
-            <input 
-              value={this.state.value}
-              onChange={event => this.setState({value: event.target.value})}
-            />
-          </div>
-          <button>Enter</button>
-        </form>
-        <hr />
-        <h4>Ready to pick a winner?</h4>
-        <button onClick={this.onClick}>Pick a winner!</button>
-        <hr />
-        <h1>{this.state.message}</h1>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h2>Lottery</h2>
+      <p>
+        This contract is managed by {manager}
+        There are currently {players.length} people enterd, competing to win{" "}
+        {web3.utils.fromWei(balance, "ether")} ether!
+      </p>
+      <hr />
+      <form onSubmit={handleSubmit}>
+        <h4>Want to try your luck?</h4>
+        <div>
+          <label> Amount of ether to enter</label>
+          <input
+            value={value}
+            onChange={(event) => setValue(event.target.value)}
+          />
+        </div>
+        <button>Enter</button>
+      </form>
+      <hr />
+      <h4>Ready to pick a winner?</h4>
+      <button onClick={handleClick}>Pick a winner!</button>
+      <hr />
+      <h1>{message}</h1>
+    </div>
+  );
 }
 export default App;
